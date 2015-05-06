61b53c7e3401e47a64544792eab605b31b4e8828
Januar1 <jan.malec@gmail.com>
2015-05-06 15:58:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jan/hintfinder94/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.jan.hintfinder94;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText sLetter, letters;
+    private Button display;
+    private SeekBar lenBar;
+    private TextView textView;
+
+    private Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sLetter = (EditText) findViewById(R.id.startingLetter);
+        letters = (EditText) findViewById(R.id.letters);
+        display = (Button) findViewById(R.id.display);
+        lenBar = (SeekBar) findViewById(R.id.lBar);
+        textView = (TextView) findViewById(R.id.sliderText);
+        context = this.getApplicationContext();
+
+
+        sLetter.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast msg = Toast.makeText(getBaseContext(), "Vpisi 1 crko",
+                        Toast.LENGTH_LONG);
+                msg.show();
+            }
+        });
+
+        textView.setText("Dolzina besede: " + lenBar.getProgress() + "/" + lenBar.getMax());
+        lenBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar lenBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                //Toast.makeText(getApplicationContext(), "Spreminjam dolzino besede", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar lenBar) {
+                //Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar lenBar) {
+                textView.setText("Dolzina besede: " + progress + "/" + lenBar.getMax());
+                //Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        display.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //String displayString = "You are looking for  r'" + sLetter.getText().toString() +
+                //"'with letters'" + letters.getText().toString() + "'";
+
+                //Toast msg = Toast.makeText(getBaseContext(), displayString,
+                //        Toast.LENGTH_LONG);
+                //msg.show();
+                Intent zadetki = new Intent(context, resultScreen.class);
+                zadetki.putExtra("sLetter", sLetter.getText().toString());
+                zadetki.putExtra("letters", letters.getText().toString());
+                zadetki.putExtra("length", lenBar.getProgress());
+                startActivity(zadetki);
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/jan/hintfinder94/resultScreen.java
@@ -0,0 +1,134 @@
+package com.example.jan.hintfinder94;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by jan on 6.5.2015.
+ */
+public class resultScreen extends Activity {
+        String sLetter;
+        String letters;
+        int length;
+        String dictionary = "";
+        List<String> arrayResults = new ArrayList<String>();
+        List<String> arrayResultsClean = new ArrayList<String>();
+        String [] listResults;
+        String abeceda = "abcdefghijklmnopqrstuwxyz";
+        int N = 25;
+
+
+
+    	protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.results_activity);
+            Bundle extras = getIntent().getExtras();
+            readResource();
+
+            sLetter = extras.getString("sLetter");
+            letters = extras.getString("letters");
+            length = extras.getInt("length");
+            String pattern = sLetter + "[" + letters + "]{" + Integer.toString(length-1) +
+                                "}\n";
+
+            int[] vIskani;
+            int[] vTrenutni;
+            vIskani = new int[N];
+            vTrenutni = new int[N];
+
+            //Toast.makeText(getApplicationContext(), "Searching for: " + pattern,
+            //        Toast.LENGTH_SHORT).show();
+
+            Pattern s_pattern = Pattern.compile(pattern);
+            Matcher s_matcher = s_pattern.matcher(dictionary);
+            int i = 0;
+            while(s_matcher.find()){
+                arrayResults.add(s_matcher.group());
+                i++;
+            }
+
+            for(i = 0; i < N; i++){
+                vIskani[i] = 0;
+            }
+            int indeks;
+
+            for(i = 0; i < letters.length(); i++){
+                indeks = abeceda.indexOf(letters.charAt(i));
+
+            //
+                if(indeks > 0) {
+                    vIskani[indeks] += 1;
+                    /*Toast.makeText(getApplicationContext(), "i: " + Integer.toString(i) +
+                            "crka: " + letters.charAt(i) + " indeks: " + Integer.toString(indeks),
+                            Toast.LENGTH_SHORT).show();
+*/
+                }
+
+            }
+            indeks = abeceda.indexOf(sLetter);
+            vIskani[indeks] += 1;
+
+
+
+            int flag;
+            for(String beseda : arrayResults){
+                flag = 0;
+                for(i = 0; i < N; i++){
+                    vTrenutni[i] = 0;
+                }
+
+                for(i = 0; i < beseda.length()-1; i++){
+                    indeks = abeceda.indexOf(beseda.charAt(i));
+                    if(indeks > 0) {
+                        vTrenutni[indeks] += 1;
+                    }
+
+                }
+                for(i = 0; i < N; i++){
+                    if(vTrenutni[i] > vIskani[i]){
+                        flag = 1;
+                    }
+                }
+                if(flag == 0){
+                    arrayResultsClean.add(beseda);
+                }
+
+            }
+
+            listResults = new String[ arrayResultsClean.size() ];
+            arrayResultsClean.toArray(listResults);
+
+            ListAdapter adapter = new ArrayAdapter<String>(resultScreen.this, android.R.layout.simple_list_item_1, listResults);
+            ListView resultsList = (ListView) findViewById(R.id.listView);
+            resultsList.setAdapter(adapter);
+
+        }
+
+        public void readResource(){
+            try {
+                InputStream fileStream = getResources().openRawResource(
+                        R.raw.dict);
+                int fileLen = fileStream.available();
+                byte[] fileBuffer = new byte[fileLen];
+                fileStream.read(fileBuffer);
+                fileStream.close();
+                dictionary = new String(fileBuffer);
+
+            } catch (IOException e) {
+            }
+
+        }
+}
